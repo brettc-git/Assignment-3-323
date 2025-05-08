a662cd3e5f0663096b03f6d86172f97be82ae9cc
Leah Trinh <40077112+ldt4me@users.noreply.github.com>
2025-05-08 01:35:54 -0700

    Update main.py lt

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from symboltable import SymbolTable
 from syntax import Syntax
 from assembly import Assembly
 
+def done(input_text):
+    table = SymbolTable()
+    AC = Assembly() # For commands used
+
+    # Create lexer and get tokens from program
+    parser = Syntax(input_text, table, AC)
+    output_content = parser.parse()
+    
+    table.printTable()
+    AC.print_instructions()
 
 def main():
     # Example input text - can be replaced with file reading if needed
@@ -16,17 +26,42 @@ def main():
     }
     $$
     """
+    done(input_text)
 
-    # Prepare symbol table 
-    table = SymbolTable()
-    AC = Assembly() # For commands used
+    input_text = """
+    $$
+    integer a, b;
+    $$
+    a = 10;
+    b = 20;
+    if (a != b) {
+        print(a);
+    }
+    if (a <= b) {
+        print(b);
+    }
+    $$
+    """
+    done(input_text)
 
-    # Create lexer and get tokens from program
-    parser = Syntax(input_text, table, AC)
-    output_content = parser.parse()
-    
-    table.printTable()
-    AC.print_instructions()
+    input_text = """
+    $$
+    integer x, y, z;
+    $$
+    x = 5;
+    y = 10;
+    z = 15;
+    if (x < y && y <= z) {
+        print(x);
+        print(y);
+        print(z);
+    }
+    if (z != x) {
+        print(z);
+    }
+    $$
+    """
+    done(input_text)
     # lexer = Lexical(input_text)
     # tokens = lexer.parse()
 
